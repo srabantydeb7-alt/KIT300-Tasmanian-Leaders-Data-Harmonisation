1341cfb9ccd03b7555bb3f29d70d5cd186d94e7d
mptyzon <4004mb@gmail.com>
2026-08-12 22:40:24 +1000

    Update DatasetsPage.js

---
+++ client/src/pages/Datasets/DatasetsPage.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./DatasetsPage.css";
+
+function DatasetsPage() {
+  return (
+    <div className="datasets-page">
+      <h1>Dataset setup and file upload</h1>
+      <p>
+        Upload source files and record the metadata required for repeatable
+        harmonisation.
+      </p>
+    </div>
+  );
+}
+
+export default DatasetsPage;
