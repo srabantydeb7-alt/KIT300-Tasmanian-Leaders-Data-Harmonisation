e20e6d28d45d9e54604688381dc4fee477bc3175
mptyzon <4004mb@gmail.com>
2026-08-12 22:43:10 +1000

    Update MappingPage.js

---
+++ client/src/pages/Mapping/MappingPage.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./MappingPage.css";
+
+function MappingPage() {
+  return (
+    <div className="mapping-page">
+      <h1>Question mapping review</h1>
+      <p>
+        Review suggested master-question matches and approve or manually map
+        uploaded questions.
+      </p>
+    </div>
+  );
+}
+
+export default MappingPage;
